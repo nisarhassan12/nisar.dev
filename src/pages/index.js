2782217213e394672076fc2748f02d1aa3d1e147
nisarhassan12 <syednisarhassan12@gmail.com>
2019-10-13 12:05:39 +0500

    Completed the basic setup: configured gatsby.

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IndexPage = () => (
+    <>
+        <p>
+            The site is currently under development.
+        </p>
+        Author: <a href="https://twitter.com/nisarhassan12">Nisar Hassan</a>
+    </>
+)
+
+export default IndexPage
