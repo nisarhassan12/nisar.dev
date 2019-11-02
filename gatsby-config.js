e77214b2ea1cce28c51f44654d2db27f58ee3c4e
nisarhassan12 <syednisarhassan12@gmail.com>
2019-11-02 20:03:45 +0500

    Created baisc Layout.

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteMetadata: {
+        title: "Nisar Hassan Naqvi - Frontend UX Engineer & Open Sourcerer.",
+        description: "Nisar Hassan Naqvi is Pakistani Front-end Frontend UX Engineer & Open Sourcerer who builds inclusive, accessible, performant and etihical products with thoughtful practices."
+    }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import LayoutIndex from '../layouts/LayoutIndex'
+
 const IndexPage = () => (
-    <>
+    <LayoutIndex canonical="/">
         <p>
             The site is currently under development.
         </p>
         Author: <a href="https://twitter.com/nisarhassan12">Nisar Hassan</a>
-    </>
+    </LayoutIndex>
 )
 
 export default IndexPage
