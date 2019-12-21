c7c6d5b608c92b7ca8571674be82a40a40c05344
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2019-12-21 05:24:42 +0000

    update site title & description.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     siteMetadata: {
-        title: "Nisar Hassan Naqvi - Frontend UX Engineer & Open Sourcerer.",
-        description: "Nisar Hassan Naqvi is Pakistani Front-end Frontend UX Engineer & Open Sourcerer who builds inclusive, accessible, performant and etihical products with thoughtful practices."
+        title: "Nisar Hassan Naqvi - Frontend UX Engineer ",
+        description: "Nisar Hassan Naqvi is Pakistani Front-end UX Engineer who builds inclusive, accessible & performant websites."
     }
 }
