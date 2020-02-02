b6746acbdbbbe90a27ec7b756051b16a4c447251
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-02-02 03:21:05 +0000

    [seo] add gatsby-plugin-react-helmet.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
         description: "A personal site for Nisar Hassan Naqvi a front end web developer and interface designer and who builds UX rich accessible & performant websites. Checkout his latest articles, projects, experiments and open source work here."
     },
     plugins: [
+        `gatsby-plugin-react-helmet`,
         `gatsby-transformer-remark`,
         {
             resolve: `gatsby-source-filesystem`,
