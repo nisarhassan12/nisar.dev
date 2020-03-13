b1e05b15258116c6c8fa59d5746884217e5fef83
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-03-13 15:23:55 +0000

    [index] About fix the grammar.

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ const IndexPage = () => (
                     <div className="wrapper">
                         <h2 className="heading--underlined">About Me</h2>
                         <p>
-                            I am a self-taught front end developer and interface designer from Pakistan.<span className="hack">&nbsp;&nbsp;&nbsp;</span> I was always curious about computers when I was a child I got introduced to programming in school. I dropped out from universty at the age of 18 with the grand plans of becoming a great computer scientist. I started my career as a front end web developer in 2019. I currently work for <a href="https://typefox.io" target="_blank">TypeFox</a> as a freelancer where entirety of the work i do is open source. These days I'm focusing on making <a href="https://theia-ide.org" target="_blank">Theia</a>(Cloud & Desktop IDE) accessible and great to use on tablets.
+                            I am a self-taught front end developer and interface designer from Pakistan.<span className="hack">&nbsp;&nbsp;&nbsp;<br/></span>I was always curious about computers. When I was a child I was introduced to programming in school. I dropped out of universty at the age of 18 with the grand plans of becoming a great computer scientist. I started my career as a front end web developer in 2019. I currently work for <a href="https://typefox.io" target="_blank">TypeFox</a> as a freelancer where the entirety of the work i do is open source. These days I'm focusing on making Theia (Cloud & Desktop IDE) accessible and great to use on tablets.
                         </p>
                     </div>
                 </div>
